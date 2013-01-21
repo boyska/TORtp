83d03a3fcaac670995fdd973bedcf88b2be0dfed
vincent <vincent@kavula.(none)>
2013-01-21 11:15:28 +0100

    install script description

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 # TORtp
 user=`id -un`
 sudo apt-get install tor -y
+
+# add custom torrc config file:
 sudo cp ./files/torrc /etc/tor/
+
+# add custom xfce-menu's entry:
 cp ./files/tortp.desktop /home/$user/.local/share/applications/
 sudo cp ./files/anonymous.ico /usr/share/pixmaps/
 chown -R $user:$user /home/$user/.local/share/applications/tortp.desktop
+
+# tortp script:
 sudo cp ./files/tortp.sh /usr/local/bin/
+
+# send notify
 notify-send TORtp "Installazione avvenuta con successo, avvia TORtp dal menu" -i /usr/share/pixmaps/anonymous.ico
